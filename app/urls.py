eb0f3ab4b9e25f2f5260e41f831a16c96bf3fa63
Jonathan Govinda Medrano Salazar <arithgrey@gmail.com>
2025-09-02 07:33:44 -0600

    [FEAT] devops tools

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,24 @@ from rest_framework.routers import DefaultRouter
 from app.landing.views import LandingTemplateViewSet, ProductLandingViewSet
 from django.http import JsonResponse
 from django.db import connection
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
 router = DefaultRouter()
 router.register(r'templates', LandingTemplateViewSet)
 router.register(r'product-landings', ProductLandingViewSet)
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Service Landings API",
+        default_version='v1',
+        description="API para gestión de plantillas y landings de productos",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 def health_view(request):
@@ -32,4 +46,7 @@ urlpatterns = [
     path('health', health_view),
     path('liveness', liveness_view),
     path('readiness', readiness_view),
+    # Swagger/OpenAPI
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
