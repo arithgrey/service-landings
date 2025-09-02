eaf4e28aba4a3233a9eb926f615d7957bfc3c050
Jonathan Govinda Medrano Salazar <arithgrey@gmail.com>
2025-09-02 07:35:14 -0600

    [FEAT] devops tools

--- test_cors.js
+++
@@ -1,9 +0,0 @@
-fetch("http://localhost:8084/api/landings/pages/", {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-  }
-})
-.then(response => response.json())
-.then(data => console.log("✅ CORS funcionando:", data))
-.catch(error => console.error("❌ Error CORS:", error));
